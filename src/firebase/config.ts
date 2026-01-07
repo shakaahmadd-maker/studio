1f41d6c5fb4564c747935aa22e4c29d375a3618f
Ishtiaq Ahmad <shaka.ahmadd@gmail.com>
2026-01-07 14:07:21 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5243418223-ccb0b",
+  "appId": "1:135918385870:web:873b63a4ead61a87312e9f",
+  "apiKey": "AIzaSyC_6gSfFZ7lv-d5Mmnb5rj8mlpZvI-ze6w",
+  "authDomain": "studio-5243418223-ccb0b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "135918385870"
+};
