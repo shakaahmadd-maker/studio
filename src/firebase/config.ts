b78efdab7bd7c71d12ba5b0f786d3e33b0a3be3d
Ishtiaq Ahmad <shaka.ahmadd@gmail.com>
2026-01-09 16:14:04 +0000

    Verify and Update Your Application's Firebase Configuration: The auth/co

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-5243418223-ccb0b",
-  "appId": "1:135918385870:web:873b63a4ead61a87312e9f",
-  "apiKey": "AIzaSyCzfvvoCMp9wqPhTtl6ikeQeOmqDtcsaG8",
-  "authDomain": "studio-5243418223-ccb0b.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "135918385870"
+  apiKey: "AIzaSyC_6gSfFZ7lv-d5Mmnb5rj8mlpZvI-ze6w",
+  authDomain: "studio-5243418223-ccb0b.firebaseapp.com",
+  projectId: "studio-5243418223-ccb0b",
+  storageBucket: "studio-5243418223-ccb0b.appspot.com",
+  messagingSenderId: "135918385870",
+  appId: "1:135918385870:web:873b63a4ead61a87312e9f"
 };
