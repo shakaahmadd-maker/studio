5f2897b2594cb3e93d13938cc1059da488a42aab
Ishtiaq Ahmad <shaka.ahmadd@gmail.com>
2026-01-09 16:21:08 +0000

    please set the following as well
    Downloading the latest Firebase Config

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyC_6gSfFZ7lv-d5Mmnb5rj8mlpZvI-ze6w",
   authDomain: "studio-5243418223-ccb0b.firebaseapp.com",
   projectId: "studio-5243418223-ccb0b",
-  storageBucket: "studio-5243418223-ccb0b.appspot.com",
+  storageBucket: "studio-5243418223-ccb0b.firebasestorage.app",
   messagingSenderId: "135918385870",
   appId: "1:135918385870:web:873b63a4ead61a87312e9f"
 };
