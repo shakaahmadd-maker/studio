4d6759e8cda610ff8d3dacad236d84969804906f
Ishtiaq Ahmad <shaka.ahmadd@gmail.com>
2026-01-09 12:16:15 +0000

    i have done with api and firestore database
    there is following api key

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-5243418223-ccb0b",
   "appId": "1:135918385870:web:873b63a4ead61a87312e9f",
-  "apiKey": "AIzaSyC_6gSfFZ7lv-d5Mmnb5rj8mlpZvI-ze6w",
+  "apiKey": "AIzaSyCzfvvoCMp9wqPhTtl6ikeQeOmqDtcsaG8",
   "authDomain": "studio-5243418223-ccb0b.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "135918385870"
